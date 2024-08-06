27fce41965022c7e08950e0e4a4153d93544155d
개는또사면돼 <139518005+TrexVsTank@users.noreply.github.com>
2024-08-06 10:29:02 +0900

    Update and rename Solution_SWEA_5432_쇠막대기자르기_D4_110ms_김민섭.java to Solution_SWEA_5432_쇠막대기자르기_D4_김민섭_110ms.java

--- Solution_SWEA_5432_쇠막대기자르기_D4_김민섭_110ms.java
+++ Solution_SWEA_5432_쇠막대기자르기_D4_김민섭_110ms.java
@@ -5,9 +5,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-// Solution_SWEA_5432_쇠막대기자르기_D4_110ms_김민섭
+// Solution_SWEA_5432_쇠막대기자르기_D4_김민섭_110ms
 
-public class Solution_SWEA_5432_쇠막대기자르기_D4_110ms_김민섭 {
+public class Solution_SWEA_5432_쇠막대기자르기_D4_김민섭_110ms {
 	public static void main(String[] args) throws IOException {
 		
 		StringBuilder sb = new StringBuilder();
